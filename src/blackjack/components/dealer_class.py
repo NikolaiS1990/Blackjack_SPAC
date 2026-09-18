de57e6602d1f9dabb0da11777ccd48abf0c76a54
NikolaiS1990 <sandbeck1990@protonmail.com>
2026-09-18 15:01:11 +0200

    begin to work on the dealer class.

--- src/blackjack/components/dealer_class.py
+++ src/blackjack/components/dealer_class.py
@@ -1,12 +1,21 @@
+from blackjack.components.hand import Hand
+from blackjack.components.cards import Cards
 
+# TODO: This could replace the round class actually.
+# The dealer starts the game, new rounds, closes the game etc.
 
 class Dealer():
 
     def __init__(self):
+        self.__name: str = "Mr. Dealer"
+        self.__dealer_hand: object = Hand()
+        self.__dealer_deck: object = Cards()
+        self.__dealer_hand.set_max_total_value(17)
 
-        cards: list = []
+    def first_round(self):
+        __dealer_deck
+        card1 = self.__dealer_deck.
+        print(self.__dealer_hand.get_hand_value())
 
-
-    def set_cards:
-    
-    
+mii = Dealer()
+mii.first_round()
